a7c1425ccea61698171f92b80535f53bd340fc39
solomon <solomonjohnbull676@gmail.com>
2025-08-31 18:07:05 +0100

    vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite';
-import path from 'path';
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 
 
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-   base:  '/Agonixtechsolutions/',
-
-
+  base: process.env.VITE_BASE || '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
 })
+
+
+
